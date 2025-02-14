9062f62e6e16506579781255e7763d3de7b286a5
Mansur <hhakerjon@gmail.com>
2025-02-14 14:30:31 +0500

    o'zimdagi oxirgi o'zgarish

--- homa_page.dart
+++ homa_page.dart
@@ -3,8 +3,3 @@ import 'chek_shablon.dart';
 import 'dart:io';
 import 'tel_raqam_ozgartirish_page.dart';
 
-void main(List<String> args) {
-  userBaza();
-  //Vaqtincha tel raqam o'zgartirish sahifasiga kirish
-  // telNumberEditing();
-}
